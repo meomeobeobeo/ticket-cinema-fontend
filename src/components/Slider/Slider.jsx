27dd27704bd31c1a096f6b530bc4d4c99888a37e
@minhduc30102001 <ducnm@jitsinnovationlabs.com>
2023-06-04 23:15:33 +0700

    update

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,5 @@
 import { Button, Image } from "antd";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from './styles.module.scss';
 import {FcNext , FcPrevious} from 'react-icons/fc'
 const listPicture = [
@@ -12,27 +12,64 @@ const listPicture = [
   {
     url: "src/assets/image/pic3.jpg",
   },
+  {
+    url: "src/assets/image/pic4.jpg",
+  },
+  {
+    url: "src/assets/image/pic5.jpg",
+  },
+
 ];
 
 const Slider = () => {
   // slider is component show list image and information of film
   const [visible, setVisible] = useState(false);
+  const [picvisible , setPicvisible] = useState(0)
+  
+  const [sliderImage , setSliderImage] = useState(listPicture[picvisible].url)
+  useEffect(()=>{
+    setSliderImage(listPicture[picvisible].url)
+  },[picvisible])
+  const handleNextPic = () => {
+    if(picvisible === listPicture.length - 1) {
+      setPicvisible(0)
+      return
+    }
+    setPicvisible(picvisible + 1)
+  }
+  const handlePrevPic = ()=>{
+    if(picvisible === 0) {
+      setPicvisible(listPicture.length - 1)
+      return
+    }
+    setPicvisible(picvisible - 1)
+  }
+
+
   return (
     <div className="w-full flex  justify-center items-center">
 
+
+      
+
       {/* previus */}
-      <Button className="m-[8px]"   >
+      <Button 
+      
+      onClick={()=>{
+        handlePrevPic()
+      }}
+      className="m-[8px]"   >
         <FcPrevious/>
       </Button>
       <Image
-      className={styles.image}
+      className={`${styles.image} `}
         preview={{
           visible: false,
         }}
-        src="src\assets\image\pic5.jpg"
+        src={sliderImage}
         onClick={() => setVisible(true)}
       />
-       <Button className="m-[8px] "   >
+       <Button onClick={()=>{handleNextPic()}} className="m-[8px] "   >
         <FcNext/>
        </Button>
       <div
